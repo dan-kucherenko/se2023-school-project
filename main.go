5d247afff2dc08458c49c667e4c129376ef4dcc9
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-05-26 14:15:59 +0300

    Router and main function completed

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"github.com/dan-kucherenko/se-school-project/currency_rate_getter"
+	"github.com/dan-kucherenko/se-school-project/router"
+	"github.com/joho/godotenv"
+	"log"
 )
 
 func main() {
-	time, rate, _ := currency_rate_getter.GetRateBtcToUah()
-	fmt.Println("Time", time)
-	fmt.Printf("Rate %f", rate)
+	loadEnvFile()
+	router.ActivateRouter()
+}
+
+// load .env file for future function calls
+func loadEnvFile() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 }
--- router/router.go
+++ router/router.go
@@ -1,22 +1,63 @@
 package router
 
 import (
+	"fmt"
+	"github.com/dan-kucherenko/se-school-project/currency_rate_getter"
+	"github.com/dan-kucherenko/se-school-project/emails"
 	"log"
 	"net/http"
 	"os"
 )
 
+// main routing function to start the server and activate the endpoints handlers
 func ActivateRouter() {
 	router := http.NewServeMux()
 
-	router.HandleFunc("/rate", rate)
-	router.HandleFunc("/subscribe", subscribe)
-	router.HandleFunc("/sendEmails", sendEmails)
+	router.HandleFunc("/api/rate", rate)
+	router.HandleFunc("/api/subscribe", subscribe)
+	router.HandleFunc("/api/sendEmails", sendEmails)
 
-	err := http.ListenAndServe(os.Getenv("PATH"), router)
+	port := os.Getenv("PORT")
+	err := http.ListenAndServe(":"+port, router)
 	log.Fatal(err)
 }
 
-func rate(rw http.ResponseWriter, r *http.Request)       {}
-func subscribe(rw http.ResponseWriter, r *http.Request)  {}
-func sendEmails(rw http.ResponseWriter, r *http.Request) {}
+// rate handler function
+func rate(rw http.ResponseWriter, _ *http.Request) {
+	time, curRate, err := currency_rate_getter.GetRateBtcToUah()
+	if err != nil {
+		rw.WriteHeader(400)
+		http.Error(rw, "Error during getting the currency rate", http.StatusBadRequest)
+		return
+	}
+	resultingString := fmt.Sprintf("Current rate at %s BTC to UAH is %f", time, curRate)
+	rw.WriteHeader(200)
+	_, _ = fmt.Fprint(rw, resultingString)
+}
+
+// subscribe handler function
+func subscribe(rw http.ResponseWriter, r *http.Request) {
+	candidateEmail := r.FormValue("email")
+	if emails.IsEmailSubscribed(candidateEmail) {
+		rw.WriteHeader(409)
+		_, _ = fmt.Fprintf(rw, "Email %s is already subscribed", candidateEmail)
+	} else {
+		err := emails.SubscribeNewEmail(candidateEmail)
+		if err != nil {
+			_, _ = fmt.Fprint(rw, "Error subscribing new email")
+			return
+		}
+		rw.WriteHeader(200)
+		_, _ = fmt.Fprintf(rw, "Email %s is successfully subscribed", candidateEmail)
+	}
+}
+
+// sendEmails handler function
+func sendEmails(rw http.ResponseWriter, _ *http.Request) {
+	err := emails.SendEmailWithRate()
+	if err != nil {
+		_, _ = fmt.Fprint(rw, "Error sending the emails with rate")
+	}
+	rw.WriteHeader(200)
+	_, _ = fmt.Fprint(rw, "Emails were successfully sent")
+}
