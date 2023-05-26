575f991f6d5140e7c4c46ae99a98b730c10f2ead
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-05-26 14:15:13 +0300

    Mail sender added

---
+++ emails/emails_sender.go
@@ -0,0 +1,38 @@
+package emails
+
+import (
+	"crypto/tls"
+	"fmt"
+	"github.com/dan-kucherenko/se-school-project/currency_rate_getter"
+	"github.com/go-gomail/gomail"
+	"os"
+)
+
+func sendEmail(emails []string, message string) error {
+	if len(emails) == 0 {
+		return nil
+	}
+	senderEmail := os.Getenv("EMAIL")
+	senderPassword := os.Getenv("EMAIL_PASSWORD")
+
+	mail := gomail.NewMessage()
+	mail.SetHeader("From", senderEmail)
+	mail.SetHeader("To", emails...)
+	mail.SetHeader("Subject", "Currency rate BTC to UAH")
+	mail.SetBody("text/html", message)
+
+	dialer := gomail.NewDialer("smtp.gmail.com", 587, senderEmail, senderPassword)
+	dialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	// send the email
+	if err := dialer.DialAndSend(mail); err != nil {
+		panic(err)
+	}
+	return dialer.DialAndSend(mail)
+}
+
+func SendEmailWithRate() error {
+	time, rate, _ := currency_rate_getter.GetRateBtcToUah()
+	emails := GetAllEmails()
+	message := fmt.Sprintf("On %v, 1 BTC costs %f UAH\n", time, rate)
+	return sendEmail(emails, message)
+}
